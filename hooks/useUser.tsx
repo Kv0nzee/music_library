aeabb2cfa8237d4b4871d7eb9f8d242fdfeebce4
Kaung Zaw Hein <kaungzawhein1963@gmail.com>
2024-02-16 22:52:26 +0630

    remove unnecessary

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -3,7 +3,7 @@
 import { User } from "@supabase/auth-helpers-nextjs"; // Importing the User type from the Supabase library
 import { useSessionContext, useUser as useSupaUser } from "@supabase/auth-helpers-react"; // Importing custom hooks for managing session and user data
 import { createContext, useContext, useEffect, useState } from "react"; // Importing necessary hooks and utilities from React
-import { Subscription, UserDetails } from "@/type"; // Importing custom types
+import { UserDetails } from "@/type"; // Importing custom types
 
 // Define the type for the context value
 type UserContextType = {
@@ -11,7 +11,6 @@ type UserContextType = {
     user: User | null;
     userDetails: UserDetails | null;
     isLoading: boolean;
-    subscription: Subscription | null;
 };
 
 // Create the UserContext using createContext, initialized with undefined
@@ -39,29 +38,21 @@ export const MyUserContextProvider = (props: Props) => {
     // State variables to manage loading state and user details
     const [isLoadingData, setIsLoadingData] = useState(false);
     const [userDetails, setUserDetails] = useState<UserDetails | null>(null);
-    const [subscription, setSubscription] = useState<Subscription | null>(null);
 
     // Function to fetch user details from the database
     const getUserDetails = () => supabase.from('users').select('*').single();
-    // Function to fetch subscription details from the database
-    const getSubscription = () => supabase.from('subscriptions').select('*, prices(*, products(*))').in('status', ['trialing', 'active']).single();
 
     // useEffect hook to fetch data when the component mounts or user state changes
     useEffect(() => {
-        if(user && !isLoadingData && !userDetails && !subscription){
+        if(user && !isLoadingData && !userDetails){
             setIsLoadingData(true);
 
-            Promise.allSettled([getUserDetails(), getSubscription()]).then(
+            Promise.allSettled([getUserDetails()]).then(
                 (results) => {
                     const userDetailsPromise = results[0];
-                    const subscriptionPromise = results[1];
 
                     if(userDetailsPromise.status === "fulfilled"){
                         setUserDetails(userDetailsPromise.value.data as UserDetails)
-                    }
-
-                    if(subscriptionPromise.status === "fulfilled" ){
-                        setSubscription(subscriptionPromise.value.data as Subscription)
                     }
 
                     setIsLoadingData(false);
@@ -69,7 +60,6 @@ export const MyUserContextProvider = (props: Props) => {
             )
         }else if(!user && !isLoadingData && !isLoadingUser){
             setUserDetails(null);
-            setSubscription(null);
         }
     }, [user, isLoadingData]);
 
@@ -79,7 +69,6 @@ export const MyUserContextProvider = (props: Props) => {
         user,
         userDetails,
         isLoading: isLoadingUser || isLoadingData,
-        subscription
     };
 
     // Render the UserContext.Provider with the value passed as a prop
